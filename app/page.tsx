2caf56411161a81c2bd405b433ead51c2ac41a44
ArchanRD <archanrd29@gmail.com>
2025-05-19 12:38:46 +0530

    Change server ip

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <h1>This next js app is deployed on EC2</h1>
+        <h1>This next js app is deployed on EC2</h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
